34f2bf0113280cada77643e88f011c01387a5373
Hops2211 <rjohns42@hawk.iit.edu>
2016-12-02 00:00:46 -0600

    merge conflict? fix

--- js/site.js
+++ js/site.js
@@ -6,12 +6,22 @@ $.noConflict();
         e.preventDefault();
         console.log('got submitted'); 
         var searched = $('#search-form').val();
+<<<<<<< HEAD
         var baseurl = 'http://api.genius.com';
         headers = {'Authorization': 'Bearer ***'};
         var searchurl = 'http://api.genius.com' + '/search?q=';
         data = {q:searched};
         response=request.get(searchurl, data=data, headers=headers);
    })
+=======
+        var baseurl ='https://api.genius.com/access_token/naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw/';
+  //      headers = {'Authorization': 'Bearer naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw'};
+        var searchurl = baseurl + 'search?q='+searched;
+   //     data = {q:searched};
+    //    response=request.get(searchurl, data=data, headers=headers);
+    $.get(searchurl);
+   });
+>>>>>>> js
    }
- )
+ );
 })(jQuery);
