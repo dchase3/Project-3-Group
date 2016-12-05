d1fe3c3e6fe355f400496701387e08b51984943d
Hops2211 <rjohns42@hawk.iit.edu>
2016-12-04 20:06:46 -0600

    fixed the .val method to actually retrieve data

--- js/site.js
+++ js/site.js
@@ -1,22 +1,25 @@
-$.noConflict();
-(function($) {
-  $(document).ready(function() {
+//$.noConflict();
+//(function($) {
+//  $(document).ready(function() {
+ $(document).ready(function() {
     $('#search-form').on('submit', function(e) {
-        alert('Clicked!');
+        var searched = $('#searchplease').val();
+//      alert('Clicked!');
         e.preventDefault();
-        console.log('got submitted'); 
-        var searched = $('#search-form').val();
-        var baseurl ='https://api.genius.com/&access_token/naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw/';
+        console.log(searched);
+        var baseurl ='https://api.genius.com/#access_token/naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw/';
   //      headers = {'Authorization': 'Bearer naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw'};
         var searchurl = baseurl + 'search?q='+searched;
    //     data = {q:searched};
     //    response=request.get(searchurl, data=data, headers=headers);
-    $.get(searchurl).done(
-      function(data,json) {
-        
-        
-    });
-   }
-  );
-  }
-})(jQuery);
+/*    $.get(
+      'https://api.github.com/users/' + searched,
+      function(data) {
+ //       $('#content').append(
+          searched
+        );
+      });
+   event.preventDefault();
+   */
+  })
+})
