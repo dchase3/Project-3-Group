f6deaf4ccbdb8d9247f5590936ca9db70a04d8a0
Hops2211 <rjohns42@hawk.iit.edu>
2016-12-04 23:07:09 -0600

    api link

--- js/site.js
+++ js/site.js
@@ -7,27 +7,29 @@
 //      alert('Clicked!');
         e.preventDefault();
         console.log(searched);
-        var baseurl ='https://itmd-361-raivenjdennisc.github.io/Project-3-Group/#access_token=naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw&state=12/';
+        var baseurl ='https://api.genius.com/oauth/authorize?client_id=3513jSnBzZdQJJc-HjcmG-ab6I2O26c6v82uMbpHtCVx-Z9sbcgytAUGcuUPuA9x&redirect_uri=https://itmd-361-raivenjdennisc.github.io/Project-3-Group/&scope=me&state=12&response_type=token/';
   //      headers = {'Authorization': 'Bearer naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw'};
         var searchurl = baseurl + 'search?q='+searched;
-   //     data = {q:searched};
-    //    response=request.get(searchurl, data=data, headers=headers);
-/*\    $.get(
-      'https://api.github.com/users/' + searched,
-      function(data) {
- //       $('#content').append(
-          searched
-        );
-      });
-   event.preventDefault();
-   */
-    $.get(searchurl,
-      function(data,json) {
+
+    $.get('https://api.genius.com/oauth/authorize?client_id=3513jSnBzZdQJJc-HjcmG-ab6I2O26c6v82uMbpHtCVx-Z9sbcgytAUGcuUPuA9x&redirect_uri=https://itmd-361-raivenjdennisc.github.io/Project-3-Group/&scope=me&state=12&response_type=code',
+      function(data,res,json) {
+        res.header("Access-Control-Allow-Origin", "*");
         $('#content').append(
-          searched
+          data.result+" "+data.url
         );
       });
  //  event.preventDefault();
    
   })
 })
+
+
+// https://api.genius.com/oauth/authorize?client_id=3513jSnBzZdQJJc-HjcmG-ab6I2O26c6v82uMbpHtCVx-Z9sbcgytAUGcuUPuA9x&redirect_uri=https://itmd-361-raivenjdennisc.github.io/Project-3-Group/&scope=me&state=12&response_type=token
+
+//https://itmd-361-raivenjdennisc.github.io/Project-3-Group/#access_token=naNsqLeRWgjgmRgtVnw-lriaHr4j4VldmMZYwMr5skyyhvt3uuXYlfeJQDd2i3RL2BPio1vBHa2rCmNGVn8hHw&state=12/'
+
+/*
+<IfModule mod_headers.c>
+    Header set Access-Control-Allow-Origin "*"
+    </IfModule>
+    */
